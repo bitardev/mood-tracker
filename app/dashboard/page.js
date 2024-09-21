d1975a29c75c0971bd5bbfa0d3b35709a67725a0
Bitar Youssef <bitardev@gmail.com>
2024-09-21 09:54:20 +0100

    Add Home page and setup the boilerplate for other components

---
+++ app/dashboard/page.js
@@ -0,0 +1,16 @@
+import Dashboard from "@/components/Dashboard";
+import Login from "@/components/Login";
+import Main from "@/components/Main";
+
+export const metadata = {
+  title: "MoodTracker • Dashboard",
+  description: "Track your daily mood every day of the year!",
+};
+export default function DashboardPage() {
+  const isAtuthenticated = false;
+  let children = <Login />;
+  if (isAtuthenticated) {
+    children = <Dashboard />;
+  }
+  return <Main>{children}</Main>;
+}
